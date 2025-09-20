0099b943c80032f5aa88a5c2c57a4c58b4a4796b
izackcosta <izackcosta97@gmail.com>
2025-09-20 19:24:25 -0300

    added tweening to project and fade in / fade out methods

--- Assets/Scripts/TransitionController.cs
+++ Assets/Scripts/TransitionController.cs
@@ -1,4 +1,13 @@
+using Cysharp.Threading.Tasks;
+using System.Collections.Generic;
 using UnityEngine;
+using PrimeTween;
+
+public enum TransitionMode 
+{
+    Normal,
+    Shutters
+}
 
 public class TransitionController : MonoBehaviour
 {
@@ -11,6 +20,13 @@ public class TransitionController : MonoBehaviour
 
     private const int SCREEN_WIDTH = 1920;
     private const int SCREEN_HEIGHT = 1080;
+    private const string FADE_AMOUNT_PROPERTY_NAME = "_FadeAmount";
+
+    private readonly Dictionary<TransitionMode, string> _transitionModes = new Dictionary<TransitionMode, string>()
+    {
+        { TransitionMode.Normal, "_TRANSITIONMODE_NORMAL" },
+        { TransitionMode.Shutters, "_TRANSITIONMODE_SHUTTERS" }
+    };
 
     private void Awake()
     {
@@ -23,6 +39,7 @@ public class TransitionController : MonoBehaviour
     {
         _spriteRenderer.sprite = Sprite.Create(_transitionTexture, new Rect(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT), new Vector2(0.5f, 0.5f));
         _transitionMaterial.SetTexture("_MainTex", _transitionTexture);
+        StartFadeOut(2, TransitionMode.Shutters, Ease.OutQuad);
     }
 
     private void InitializeTransitionTexture() 
@@ -40,6 +57,36 @@ public class TransitionController : MonoBehaviour
             pixels[i] = Color.black;
         }
         return pixels;
+    }
+
+    public void StartFadeIn(float duration, TransitionMode mode, Ease ease) => FadeIn(duration, mode, ease).Forget();
+
+    private async UniTask FadeIn(float duration, TransitionMode mode, Ease ease)
+    {
+        await Tween.Custom(1, 0, duration, value => _transitionMaterial.SetFloat(FADE_AMOUNT_PROPERTY_NAME, value), ease);
+    }
+
+    public void StartFadeOut(float duration, TransitionMode mode, Ease ease) => FadeOut(duration, mode, ease).Forget();
+
+    private async UniTask FadeOut(float duration, TransitionMode mode, Ease ease)
+    {
+        SetTransitionMode(mode);
+        await Tween.Custom(0, 1, duration, value => _transitionMaterial.SetFloat(FADE_AMOUNT_PROPERTY_NAME, value), ease);
+    }
+
+    private void SetTransitionMode(TransitionMode mode)
+    {
+        foreach (var kvp in _transitionModes)
+        {
+            if (kvp.Key == mode)
+            {
+                _transitionMaterial.EnableKeyword(kvp.Value);
+            }
+            else
+            {
+                _transitionMaterial.DisableKeyword(kvp.Value);
+            }
+        }
     }
 
 }
